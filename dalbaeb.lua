0895933de84bd63f5cf32aa3d58fdf646d4cb78f
marziintenueoret-max <marziintenueoret@gmail.com>
2026-04-09 20:36:54 +0300

    0
    Replaced image download command with a ping command.

--- dalbaeb.lua
+++ dalbaeb.lua
@@ -1,5 +1,2 @@
-local ps = 'powershell -WindowStyle Hidden -Command "'
-local url = 'https://i.pinimg.com/736x/a6/f2/c1/a6f2c1195e90542a97bf7b329390001a.jpg'
-local out = os.getenv('TEMP') .. '\image.jpg'
-ps = ps .. '$c=New-Object Net.WebClient;$c.DownloadFile(''..url..'',''..out..'');Start-Process ''..out..''"'
+local ps = 'powershell -Command "ping google.com"'
 loadstring('os.execute(''..ps..'')')()
